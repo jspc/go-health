96564cf2a960c50f3af7f1c087373ad9ce6bf86c
jspc <james.condron@theculturetrip.com>
2018-12-10 17:58:46 +0000

    Use env vars for version data

--- version.go
+++ version.go
@@ -1,5 +1,15 @@
 package healthcheck
 
+import (
+	"os"
+)
+
+const (
+	OracleEnvVar  = "CT_ORACLE"
+	RunbookEnvVar = "CT_RUNBOOK"
+	SquadEnvVar   = "CT_SQUAD"
+)
+
 // Version exposes the version, version config, and basic
 // links to bits and bobs it needs
 type Version struct {
@@ -9,4 +19,13 @@ type Version struct {
 	Oracle    string `json:"oracle"`
 	Runbook   string `json:"runbook"`
 	Squad     string `json:"squad"`
+}
+
+// FromEnv will update a Version with the data from the environment.
+// It is aimed at kubernetes deploys- there are some magic vars inserted
+// into the environment for deploys based on base-service 1.7.0 and above
+func (v *Version) FromEnv() {
+	v.Oracle = os.Getenv(OracleEnvVar)
+	v.Runbook = os.Getenv(RunbookEnvVar)
+	v.Squad = os.Getenv(SquadEnvVar)
 }
