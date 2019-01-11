9e180aa70a3c2a436dec3c9874ab30f36370706f
jspc <james@zero-internet.org.uk>
2019-01-11 09:10:45 +0000

    mode models out to speed runbooks

--- healthcheck.go
+++ healthcheck.go
@@ -1,47 +1,9 @@
 package healthcheck
 
 import (
-	"time"
+	"github.com/jspc/go-health/models"
 )
 
-// Healthcheck represents an individual healthcheck
-type Healthcheck struct {
-	// Name is a simple string to help explain the point of the healthcheck
-	// It doesn't have to even be unique- it's just to make output more
-	// readable
-	Name string `json:"name"`
-
-	// Readiness and Liveness determines what check endpoints are affected by
-	// what healthchecks- it's also possible to set these as both false- this
-	// will, essentially, make a healthcheck advisory only
-	Readiness bool `json:"readiness"`
-	Liveness  bool `json:"liveness"`
-
-	// RunbookItem should point to the url and anchor, where possible, of
-	// the healthcheck/ dependency. It may be in the runbook of the service
-	// or it may be another runbook
-	RunbookItem string `json:"runbook_item"`
-
-	// F is the function that a healthcheck runs. It returns true for a
-	// successful healthcheck, and false for a failing healthcheck.
-	// It also returns optional output.
-	F func() (bool, interface{}) `json:"-"`
-
-	// The following are overwritten when a healthcheck is run
-	State    string      `json:"state"` // enum: not_run, running, run
-	LastRun  time.Time   `json:"last_run"`
-	Duration float64     `json:"duration_ms"`
-	Success  bool        `json:"success"`
-	Output   interface{} `json:"output"`
-}
-
-// Run will.... run the healthcheck
-func (h *Healthcheck) Run() {
-	h.State = "running"
-
-	h.LastRun = time.Now()
-	h.Success, h.Output = h.F()
-	h.Duration = float64(time.Since(h.LastRun)) / 1000000.0
-
-	h.State = "run"
-}
+// Healthcheck wraps a healthcheck model to be a top level export
+// of this package, for backwards compatability
+type Healthcheck models.Healthcheck
